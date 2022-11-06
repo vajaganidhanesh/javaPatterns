e9bd244aa97b1f3b03d9f24f7647c4cfbe09783f
vajagani dhanesh <vajaganidhanesh@gmail.com>
2022-11-06 21:58:21 +0530

    reversing an array

--- src/Input.java
+++ src/Input.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class Input {
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Input {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int arr[] = new int[5];
+        String arr1[] = new String[4];
+
+        arr[0] = 34;
+
+            for (int i = 0; i < arr1.length; i++) {
+                arr1[i] = in.next();
+            }
+
+            System.out.println(Arrays.toString(arr1));
+
+//            for (int i = 0; i < arr1.length; i++) {
+//                System.out.print(arr1[i] + " ");
+//            }
+
+//            for (int j : arr) {
+//                System.out.print(j + " ");
+//            }
+        }
 }
--- src/twod_Array.java
+++ src/twod_Array.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class twod_Array {
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class twod_Array {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int arr[][] = new int[3][3];
+
+        for (int row = 0; row < arr.length; row++) {
+            for (int column = 0; column < arr[row].length; column++) {
+                arr[row][column] = in.nextInt();
+            }
+        }
+        for (int[] ints : arr) {
+            System.out.println(Arrays.toString(ints));
+        }
+
+//        for (int i = 0; i < arr.length; i++) {
+//            System.out.println(Arrays.toString(arr[i]));
+//        }
+
+//        for (int i = 0; i < arr.length; i++) {
+//            for (int j = 0; j < arr[i].length; j++) {
+//                System.out.print(arr[i][j]+ " ");
+//            }
+//            System.out.println();
+//        }
+    }
 }
